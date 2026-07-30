e9da32a0c189d97a2468c58b95bc4b897440a491
amru0920 <muhdammar758@gmail.com>
2026-07-30 16:00:44 +0800

    Update branding and tournament details for Terbuka Putrajaya 2026

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 export const SUPABASE_URL  = "https://nucbgzakkgbrfmyrbyuy.supabase.co";
 export const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51Y2JnemFra2dicmZteXJieXV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxMzIyMTAsImV4cCI6MjEwMDcwODIxMH0.rAcBP_YFFS6DI2SAI7Gzl2bI8eJkvDMwXekBU0HVljQ";
 
-export const TOURNAMENT = "Kejohanan Bola Baling MSSPP 2026";
+export const TOURNAMENT = "Kejohanan Bola Baling Terbuka Putrajaya 2026";
